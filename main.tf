c74df6dcaca8897b8b1d4a8cd9c2a490df93c403
Aditi Agarwal <29581548+aditiagarwal1@users.noreply.github.com>
2024-04-05 14:46:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ data "aws_iam_session_context" "current" {
   # For non-role ARNs, this data source simply passes the ARN through issuer ARN
   # Ref https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2327#issuecomment-1355581682
   # Ref https://github.com/hashicorp/terraform-provider-aws/issues/28381
+  # comment
   arn = data.aws_caller_identity.current.arn
 }
 
